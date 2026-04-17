52463cbe60eac411fb556d7347d7988d00223613
Gabriel Lopasso <gabriellopasso@gmail.com>
2026-04-16 21:21:32 -0300

    feat: backend, API github

---
+++ backend/src/services/githubService.ts
@@ -0,0 +1,47 @@
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+import { Octokit } from "octokit";
+
+interface GitHubRepoResponse {
+  id: number;
+  name: string;
+  full_name: string;
+  visibility: string;
+  html_url: string;
+  description: string;
+  language: string;
+}
+
+interface RepoFiltrado {
+  id: number;
+  name: string;
+  full_name: string;
+  visibility: string;
+  html_url: string;
+  description: string;
+  language: string;
+}
+
+const octokit = new Octokit();
+
+const res = await octokit.request("GET /users/gabriellopasso/repos", {
+  username: "gabriellopasso",
+  headers: {
+    "X-GitHub-Api-Version": "2026-03-10",
+  },
+});
+//console.log(res.data);
+
+const repositoriosBrutos: GitHubRepoResponse[] = res.data;
+
+const reposFiltrados: RepoFiltrado[] = repositoriosBrutos.map((repo) => ({
+  id: repo.id,
+  name: repo.name,
+  full_name: repo.full_name,
+  visibility: repo.visibility,
+  html_url: repo.html_url,
+  description: repo.description,
+  language: repo.language,
+}));
+
+console.log(reposFiltrados);
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./style.module.css";
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Header({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
+}
